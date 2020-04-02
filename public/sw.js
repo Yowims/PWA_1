31e59448d737fbb65bef14602b82d1efa5c560bd
Doryan LOPEZ <lopezdoryan@gmail.com>
2020-04-02 14:28:33 +0200

    Initial commit

---
+++ public/sw.js
@@ -0,0 +1,55 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/images/transmutation-humaine-192.png',
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Cache ouvert');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+
+self.addEventListener('activate', function(event){
+
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          // IMPORTANT: Cloner la requête.
+          // Une requete est un flux et est à consommation unique
+          // Il est donc nécessaire de copier la requete pour pouvoir l'utiliser et la servir
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
+            function(response) {
+              if (!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Même constat qu'au dessus, mais pour la mettre en cache
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+    );
+  });
