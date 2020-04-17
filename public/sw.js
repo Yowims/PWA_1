a39503684ecb73f14cc3a207c338503892ab4dd7
Doryan LOPEZ <lopezdoryan@gmail.com>
2020-04-17 15:51:57 +0200

    Test de Service Worker

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,6 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   "/PWA_1/views/index.html",
-  "/PWA_1/images/transmutation-humaine-192.png",
-  "/PWA_1/manifest.webmanifest"
 ];
 
 self.addEventListener('install', (evt) => {
