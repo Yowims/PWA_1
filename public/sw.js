941350df2e226c21e2907d2d3c7f017895b10b52
Doryan LOPEZ <lopezdoryan@gmail.com>
2020-04-17 14:50:37 +0200

    Test de Service Worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
+  './',
   './views/index.html',
   './public/images/transmutation-humaine-192.png',
   './manifest.webmanifest',
@@ -7,20 +8,17 @@ var urlsToCache = [
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
-  // CODELAB: Precache static resources here.
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(urlsToCache);
     })
   );
-
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
@@ -35,13 +33,17 @@ self.addEventListener('activate', (evt) => {
 });
 
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request)
-      .then((response) => {
-        return response || fetch(evt.request);
-      });
-    })
-  );
-});
+	console.log('[ServiceWorker] Fetch', evt.request.url);
+	if (evt.request.mode !== 'navigate') {
+	  return;
+	}
+	evt.respondWith(
+	  fetch(evt.request)
+		.catch(() => {
+		  return caches.open(CACHE_NAME)
+			.then((cache) => {
+			  return cache.match('./views/index.html');
+			});
+		  })
+		);
+	});
