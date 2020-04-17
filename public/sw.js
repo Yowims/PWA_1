1715aea02d7e99279b417f64403a91369eedf2ff
Yowims <lopezdoryan@gmail.com>
2020-04-17 13:06:04 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -13,10 +13,22 @@ self.addEventListener('install', function(event) {
           return cache.addAll(urlsToCache);
         })
     );
-  });
+});
 
 self.addEventListener('activate', function(event){
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
 
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', function(event) {
